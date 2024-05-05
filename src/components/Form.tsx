7c1a67a1b6adf9eab0b7d7230b3d800e59111234
Rezmii <bartoszrezmer20@gmail.com>
2024-05-05 11:11:12 +0200

    added form validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,29 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description must have at least 3 characters" }),
+  amount: z
+    .number({ invalid_type_error: "Amount field is required." })
+    .min(1, { message: "Amount can't be 0" }),
+  category: z
+    .string()
+    .min(3, { message: "Description must have at least 3 characters" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -19,17 +41,23 @@ const Form = () => {
           className="form-control"
           id="description"
         />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
         <input
-          {...register("amount")}
+          {...register("amount", { valueAsNumber: true })}
           type="number"
           className="form-control"
           id="amount"
         />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
@@ -41,6 +69,9 @@ const Form = () => {
           className="form-control"
           id="category"
         />
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
