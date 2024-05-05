874bb4de0cf3d43f77133d3f2f091f88d4c9552a
Rezmii <bartoszrezmer20@gmail.com>
2024-05-05 11:57:27 +0200

    added new component which displays expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
+  const [expenseList, setExpenseList] = useState([
+    {
+      description: "test",
+      amount: 2,
+      category: "groceries",
+    },
+    {
+      description: "test1",
+      amount: 4,
+      category: "groceries",
+    },
+  ]);
   return (
     <>
       <Form></Form>
+      <ExpenseList expenseList={expenseList}></ExpenseList>
     </>
   );
 }
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,67 @@
+import { ChangeEvent, useState } from "react";
+
+interface Expense {
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenseList: Expense[];
+}
+
+const ExpenseList = ({ expenseList }: Props) => {
+  const [category, setCategory] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCategory(event.target.value);
+  };
+
+  const showTable = () => {
+    return (
+      category === "groceries" && (
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Category</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenseList.map((expense, index) => (
+              <tr key={index}>
+                <td>{expense.description}</td>
+                <td>{expense.amount}</td>
+                <td>{expense.category}</td>
+                <td>
+                  <button>xd</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    );
+  };
+  return (
+    <>
+      <select
+        className="form-select"
+        id="category"
+        value={category}
+        onChange={handleChange}
+      >
+        <option value=""></option>
+        <option value="all">All</option>
+        <option value="groceries">Groceries</option>
+        <option value="utilities">Utilities</option>
+        <option value="entertainment">Entertainment</option>
+      </select>
+      {showTable()}
+    </>
+  );
+};
+
+export default ExpenseList;
